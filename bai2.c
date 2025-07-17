babc09bd75746604b745274ff120b13b26a41713
pthongnhung <pthongnhung13122006@gmail.com>
2025-07-17 09:56:51 +0700

    add

--- bai2.c
+++ bai2.c
@@ -1,21 +1,67 @@
 #include <stdio.h>
 #include<stdlib.h>
+typedef struct Operation
+{
+    char url[100];
+    char Timestamp[20];
+}Operation;
+typedef struct Stack
+{
+    Operation Data[100];
+    int top;
+    int capacity;
+}Stack;
+typedef struct Queue
+{
+    Operation Data[100];
+    int front;
+    int rear;
+    int capacity;
+
+}Queue;
+Stack* createStack(int capacity)
+{
+    Stack* stack = (Stack*)malloc(sizeof(Stack));
+    stack->capacity = capacity;
+    stack->top = -1;
+    return stack;
+}
+Queue* createQueue(int capacity)
+{
+    Queue* queue = (Queue*)malloc(sizeof(Queue));
+    queue->capacity = capacity;
+    queue->front = 0;
+    queue->rear = -1;
+    return queue;
+}
+// Stack* push(Stack* stack, Operation data)
+// {
+//     if (stack->top == stack->capacity-1)
+//     {
+//         printf("FUll stack");
+//         return stack;
+//     }
+//     stack->top=stack->top+1;
+//     return stack->Data[top];
+// }
 int main()
 {
     int choice;
     do
     {
-        printf("----------ROWSER HISTORY----------");
-        printf("1.VISIT");
-        printf("2.BACK");
-        printf("3.FORWARD");
-        printf("4,HISTORY");
-        printf("5.EXIT");
+        printf("----------ROWSER HISTORY----------\n");
+        printf("1.VISIT\n");
+        printf("2.BACK\n");
+        printf("3.FORWARD\n");
+        printf("4,HISTORY\n");
+        printf("5.EXIT\n");
         printf("Nhap lua chon cua ban: ");
         scanf("%d",&choice);
         switch(choice)
         {
         case 1:
+            Operation stack;
+
             break;
         case 2:
             break;
